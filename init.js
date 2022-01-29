ca0c506c8d6bb6b7ea58dca536aa36a5a5d4fffe
Bouquet Cédric <cedric-bouquet-7@outlook.fr>
2022-01-29 21:37:34 +0100

    update

--- init.js
+++ init.js
@@ -1,10 +1,8 @@
 function init_files() {
     new Fichier("base.py",
         "jnqpsu!tzt\vefg!cbtf)dibs;tus-oc;jou*;\v\nds>pse)dibs*\v\nunq!>!ds\v\nsft!>!##\v\nxijmf!unq!\">!1;\v\n\ns!>!unq!&!oc\v\n\nunq!>!unq!00!oc\v\n\njg!s!=!21;\v\n\n\nsft!,>!tus)s*\v\n\nfmtf;\v\n\n\nsft!,>!dis)76,)s.21**\v\nsft!>!sft\\;;.2^\v\njg!mfo)sft*!=!4;\v\n\ngps!j!jo!sbohf)1-4.mfo)sft**;\v\n\n\nsft!>!#1#,sft\v\nsfuvso!sft\v\vefg!hfu)bmqib;tus-dpef;tus*;\v\nsft!>!##\v\ngps!j!jo!dpef;\v\n\njg!j/jtejhju)*;\v\n\n\nsft!,>!bmqib\\jou)j*^\v\n\nfmtf;\v\n\n\nsft!,>!bmqib\\)pse)j*.66*^\v\nsfuvso!sft\v\vbmqib>#BCDEFGHIJKLMNOPQRSTUVWXYZ[bcdefghijklmnopqrstuvwxyz{123456789:#\vtj{f!>!mfo)bmqib*\vjg!tzt/bshw\\2^!>>!#.d#;\v\nsft!>!##\v\ngps!j!jo!tzt/bshw\\3^;\v\n\ndpef!>!)cbtf)j-tj{f**\v\n\nsft!,>!hfu)bmqib-dpef*\v\nqsjou)g#|sft~#*\vfmjg!tzt/bshw\\2^!>>!#.e#;\v\nefd>##\v\ngps!j!jo!sbohf)1-mfo)tzt/bshw\\3^*-4*;\v\n\ndpef>tzt/bshw\\3^\\j;j,4^\v\n\nunq!>!1\v\n\ngps!k!jo!sbohf)mfo)dpef**;\v\n\n\njoe!>!bmqib/joefy)dpef\\k^*\v\n\n\nunq!,>!)joe!+!)tj{f!++!)mfo)dpef*.2.k***\v\n\nefd,>dis)unq*\v\nqsjou)g#|efd~#*\v",
-        "text/py",
-        "pyfile");
+        "text/py");
     new Fichier("message pour ludo.txt",
         "Ifmmp!Mvep-!\vdpnnfou!wbt.uv!@\vuv!bt!sfèv!npo!nbjm!@",
-        "text/txt",
-        "txtfile").setDestination("MdM93sM3");
+        "text/txt").setDestination("MdM93sM3");
 }
